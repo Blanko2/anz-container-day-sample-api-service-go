649769a21005cda33278d442b0d0e321a5f89bdd
Blanko2 <rodrigo.lomonaco@gmail.com>
2020-06-16 10:38:27 +1200

    Update main.go
    changing text

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World"}
+		res := &response{Message: "Testing out the World"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
